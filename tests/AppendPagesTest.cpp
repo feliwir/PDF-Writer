f23d26896f44346005df145aa2294f511a1517d5
Stephan Vedder <stephan.vedder@gmail.com>
2021-05-14 21:13:18 +0200

    Start migrating some tests

---
+++ tests/AppendPagesTest.cpp
@@ -0,0 +1,56 @@
+/*
+   Source File : AppendPagesTest.cpp
+
+
+   Copyright 2011 Gal Kahana PDFWriter
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
+*/
+#include "PDFWriter.h"
+#include "TestHelper.h"
+#include <gtest/gtest.h>
+
+#include <iostream>
+
+using namespace PDFHummus;
+
+TEST(Basic, AppendPages)
+{
+    EStatusCode status;
+    PDFWriter pdfWriter;
+
+    status = pdfWriter.StartPDF(
+        RelativeURLToLocalPath(PDFWRITE_BINARY_PATH, "AppendPagesTest.pdf"), ePDFVersion13,
+        LogConfiguration(true, true, RelativeURLToLocalPath(PDFWRITE_BINARY_PATH, "AppendPagesTestLog.txt")));
+
+    ASSERT_EQ(status, PDFHummus::eSuccess);
+
+    EStatusCodeAndObjectIDTypeList result;
+
+    result = pdfWriter.AppendPDFPagesFromPDF(RelativeURLToLocalPath(PDFWRITE_SOURCE_PATH, "TestMaterials/Original.pdf"),
+                                             PDFPageRange());
+    ASSERT_EQ(result.first, PDFHummus::eSuccess);
+
+    result = pdfWriter.AppendPDFPagesFromPDF(
+        RelativeURLToLocalPath(PDFWRITE_SOURCE_PATH, "TestMaterials/XObjectContent.pdf"), PDFPageRange());
+    ASSERT_EQ(result.first, PDFHummus::eSuccess);
+
+    result = pdfWriter.AppendPDFPagesFromPDF(
+        RelativeURLToLocalPath(PDFWRITE_SOURCE_PATH, "TestMaterials/BasicTIFFImagesTest.pdf"), PDFPageRange());
+    ASSERT_EQ(result.first, PDFHummus::eSuccess);
+
+    status = pdfWriter.EndPDF();
+    ASSERT_EQ(status, PDFHummus::eSuccess);
+}
---
+++ tests/TestHelper.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <filesystem>
+
+static std::string RelativeURLToLocalPath(const std::string& inFileURL,const std::string& inRelativeURL)
+{
+    return std::filesystem::path(inFileURL) / inRelativeURL;
+}
