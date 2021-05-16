85d7a9010fcf5e38d759881bb80660576a4af63f
Stephan Vedder <stephan.vedder@gmail.com>
2021-05-16 14:57:33 +0200

    Port more tests

---
+++ tests/AppendingAndReadingTest.cpp
@@ -0,0 +1,90 @@
+/*
+ Source File : AppendingAndReading.cpp
+
+
+ Copyright 2012 Gal Kahana PDFWriter
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+
+ */
+#include "TestHelper.h"
+#include "io/IByteReaderWithPosition.h"
+#include "io/IByteWriterWithPosition.h"
+#include "io/InputFile.h"
+#include "io/OutputFile.h"
+
+#include <gtest/gtest.h>
+#include <iostream>
+
+using namespace PDFHummus;
+
+static const Byte scTest[5] = {'T', 'e', 's', 't', '\n'};
+static const Byte scTest1[6] = {'T', 'e', 's', 't', '1', '\n'};
+static const Byte scTest2[6] = {'T', 'e', 's', 't', '2', '\n'};
+
+TEST(Modification, AppendingAndReading)
+{
+    // checks if i can create a file, and then both read and write to it at the same
+    // time with different classes. The test aims only for the scenario of reading from the part
+    // that is not written in, as would be the case for PDFs with incremental updates
+
+    EStatusCode status = eSuccess;
+
+    // write initial file
+    {
+        OutputFile outputFile;
+
+        status = outputFile.OpenFile(RelativeURLToLocalPath(PDFWRITE_BINARY_PATH, "ReadAndWrite.txt"));
+        ASSERT_EQ(status, PDFHummus::eSuccess);
+
+        outputFile.GetOutputStream()->Write(scTest, 5);
+        outputFile.GetOutputStream()->Write(scTest1, 6);
+        outputFile.GetOutputStream()->Write(scTest2, 6);
+    }
+
+    // now open file for read and file for append
+
+    InputFile inputFile;
+
+    status = inputFile.OpenFile(RelativeURLToLocalPath(PDFWRITE_BINARY_PATH, "ReadAndWrite.txt"));
+    ASSERT_EQ(status, PDFHummus::eSuccess);
+
+    OutputFile appendToFile;
+
+    status = appendToFile.OpenFile(RelativeURLToLocalPath(PDFWRITE_BINARY_PATH, "ReadAndWrite.txt"), true);
+    ASSERT_EQ(status, PDFHummus::eSuccess);
+
+    // check reading 1
+    inputFile.GetInputStream()->SetPosition(5);
+    Byte bufferTest1[6];
+
+    inputFile.GetInputStream()->Read(bufferTest1, 6);
+    ASSERT_EQ(memcmp(bufferTest1, scTest1, 6), 0);
+
+    // check writing 1
+    appendToFile.GetOutputStream()->Write(scTest1, 6);
+
+    // check reading 2
+    inputFile.GetInputStream()->Read(bufferTest1, 6);
+    ASSERT_EQ(memcmp(bufferTest1, scTest2, 6), 0);
+
+    // check reading 3
+    inputFile.GetInputStream()->SetPosition(0);
+    Byte bufferTest[5];
+    inputFile.GetInputStream()->Read(bufferTest, 5);
+    ASSERT_EQ(memcmp(bufferTest, scTest, 5), 0);
+
+    // check writing 2
+    appendToFile.GetOutputStream()->Write(scTest1, 6);
+}
---
+++ tests/BasicModificationTest.cpp
@@ -0,0 +1,185 @@
+/*
+ Source File : BasicModification.cpp
+
+
+ Copyright 2012 Gal Kahana PDFWriter
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+
+ */
+#include "PDFPage.h"
+#include "PDFRectangle.h"
+#include "PDFWriter.h"
+#include "PageContentContext.h"
+#include "TestHelper.h"
+#include "io/InputFile.h"
+#include "io/OutputFile.h"
+#include "io/OutputStreamTraits.h"
+
+#include <gtest/gtest.h>
+#include <iostream>
+
+using namespace PDFHummus;
+
+EStatusCode TestBasicFileModification(const std::string &inSourceFileName)
+{
+    PDFWriter pdfWriter;
+    EStatusCode status = eSuccess;
+
+    status = pdfWriter.ModifyPDF(
+        RelativeURLToLocalPath(PDFWRITE_SOURCE_PATH, std::string("data/") + inSourceFileName + std::string(".pdf")),
+        ePDFVersion13,
+        RelativeURLToLocalPath(PDFWRITE_BINARY_PATH, std::string("Modified") + inSourceFileName + std::string(".pdf")),
+        LogConfiguration(true, true,
+                         RelativeURLToLocalPath(PDFWRITE_BINARY_PATH,
+                                                std::string("Modified") + inSourceFileName + std::string(".log"))));
+    if (status != PDFHummus::eSuccess)
+        return status;
+
+    PDFPage *page = new PDFPage();
+    page->SetMediaBox(PDFRectangle(0, 0, 595, 842));
+
+    PageContentContext *contentContext = pdfWriter.StartPageContentContext(page);
+    if (nullptr == contentContext)
+        return PDFHummus::eFailure;
+
+    PDFUsedFont *font = pdfWriter.GetFontForFile(RelativeURLToLocalPath(PDFWRITE_SOURCE_PATH, "data/fonts/couri.ttf"));
+    if (!font)
+        return PDFHummus::eFailure;
+
+    // Draw some text
+    contentContext->BT();
+    contentContext->k(0, 0, 0, 1);
+
+    contentContext->Tf(font, 1);
+
+    contentContext->Tm(30, 0, 0, 30, 78.4252, 662.8997);
+
+    EStatusCode encodingStatus = contentContext->Tj("about");
+    if (encodingStatus != PDFHummus::eSuccess)
+        std::cout << "Could not find some of the glyphs for this font";
+
+    // continue even if failed...want to see how it looks like
+    contentContext->ET();
+
+    status = pdfWriter.EndPageContentContext(contentContext);
+    if (status != PDFHummus::eSuccess)
+        return status;
+
+    status = pdfWriter.WritePageAndRelease(page);
+    if (status != PDFHummus::eSuccess)
+        return status;
+
+    return pdfWriter.EndPDF();
+}
+
+EStatusCode TestInPlaceFileModification(const std::string &inSourceFileName)
+{
+    PDFWriter pdfWriter;
+    EStatusCode status = eSuccess;
+    // first copy source file to target
+    {
+        InputFile sourceFile;
+
+        status = sourceFile.OpenFile(RelativeURLToLocalPath(
+            PDFWRITE_SOURCE_PATH, std::string("data/") + inSourceFileName + std::string(".pdf")));
+        if (status != PDFHummus::eSuccess)
+            return status;
+
+        OutputFile targetFile;
+
+        status = targetFile.OpenFile(RelativeURLToLocalPath(
+            PDFWRITE_BINARY_PATH, std::string("InPlaceModified") + inSourceFileName + std::string(".pdf")));
+        if (status != PDFHummus::eSuccess)
+            return status;
+
+        OutputStreamTraits traits(targetFile.GetOutputStream());
+        status = traits.CopyToOutputStream(sourceFile.GetInputStream());
+        if (status != PDFHummus::eSuccess)
+            return status;
+
+        sourceFile.CloseFile();
+        targetFile.CloseFile();
+    }
+
+    // now modify in place
+
+    status = pdfWriter.ModifyPDF(
+        RelativeURLToLocalPath(PDFWRITE_BINARY_PATH,
+                               std::string("InPlaceModified") + inSourceFileName + std::string(".pdf")),
+        ePDFVersion13, "",
+        LogConfiguration(true, true,
+                         RelativeURLToLocalPath(PDFWRITE_BINARY_PATH, std::string("InPlaceModified") +
+                                                                          inSourceFileName + std::string(".log"))));
+    if (status != PDFHummus::eSuccess)
+        return status;
+
+    PDFPage *page = new PDFPage();
+    page->SetMediaBox(PDFRectangle(0, 0, 595, 842));
+
+    PageContentContext *contentContext = pdfWriter.StartPageContentContext(page);
+    if (!contentContext)
+        return PDFHummus::eFailure;
+
+    PDFUsedFont *font = pdfWriter.GetFontForFile(RelativeURLToLocalPath(PDFWRITE_SOURCE_PATH, "data/fonts/couri.ttf"));
+    if (!font)
+        return PDFHummus::eFailure;
+
+    // Draw some text
+    contentContext->BT();
+    contentContext->k(0, 0, 0, 1);
+
+    contentContext->Tf(font, 1);
+
+    contentContext->Tm(30, 0, 0, 30, 78.4252, 662.8997);
+
+    EStatusCode encodingStatus = contentContext->Tj("about");
+    if (encodingStatus != PDFHummus::eSuccess)
+        std::cout << "Could not find some of the glyphs for this font";
+
+    // continue even if failed...want to see how it looks like
+    contentContext->ET();
+
+    status = pdfWriter.EndPageContentContext(contentContext);
+    if (status != PDFHummus::eSuccess)
+        return status;
+
+    status = pdfWriter.WritePageAndRelease(page);
+    if (status != PDFHummus::eSuccess)
+        return status;
+
+    status = pdfWriter.EndPDF();
+
+    return status;
+}
+
+TEST(Modification, BasicModification)
+{
+    // modification of different source and target
+    ASSERT_EQ(TestBasicFileModification("BasicTIFFImagesTest"), eSuccess);
+    ASSERT_EQ(TestBasicFileModification("Linearized"), eSuccess);
+    ASSERT_EQ(TestBasicFileModification("MultipleChange"), eSuccess);
+    ASSERT_EQ(TestBasicFileModification("RemovedItem"), eSuccess);
+    ASSERT_NE(TestBasicFileModification("Protected"), eSuccess);
+    ASSERT_EQ(TestBasicFileModification("ObjectStreams"), eSuccess);
+    ASSERT_EQ(TestBasicFileModification("ObjectStreamsModified"), eSuccess);
+
+    // in place modification
+    ASSERT_EQ(TestInPlaceFileModification("BasicTIFFImagesTest"), eSuccess);
+    ASSERT_EQ(TestInPlaceFileModification("Linearized"), eSuccess);
+    ASSERT_EQ(TestInPlaceFileModification("MultipleChange"), eSuccess);
+    ASSERT_EQ(TestInPlaceFileModification("RemovedItem"), eSuccess);
+    ASSERT_EQ(TestInPlaceFileModification("ObjectStreams"), eSuccess);
+    ASSERT_EQ(TestInPlaceFileModification("ObjectStreamsModified"), eSuccess);
+}
